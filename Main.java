85f75162713ebc21fe9a8ac7d3b6376c90822912
Swetha03T <168464539+Swetha03T@users.noreply.github.com>
2024-05-17 19:04:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+package Ipl;
+import Ipl.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Srh s=new Srh();
+	s.batsman();
+	s.bowler();
+	s.allrounder();
+	Csk c=new Csk();
+	c.batsman();
+	c.bowler();
+	c.allrounder();
+	Rcb r=new Rcb();
+	r.batsman();
+	r.bowler();
+	r.allrounder();
+
+	}
+
+}
---
+++ Srh.java
@@ -0,0 +1,55 @@
+package Ipl;//package Ipl
+
+public class Srh 
+{
+void bowler()
+{
+	System.out.println("cummines");
+}
+void batsman()
+{
+	System.out.println("head");
+
+}
+void allrounder()
+{
+	System.out.println("abhishek");
+}
+
+}
+class Csk
+{
+	void bowler()
+	{
+		System.out.println("deepak");
+	}
+	void batsman()
+	{
+		System.out.println("dhoni");
+
+	}
+	void allrounder()
+	{
+		System.out.println("jaddu");
+
+	}
+}
+class Rcb
+{
+	void bowler()
+	{
+		System.out.println("siraj");
+	}
+	void batsman()
+	{
+		System.out.println("virat");
+
+	}
+	void allrounder()
+	{
+		System.out.println("tom curran");
+
+	}
+}
+
+
